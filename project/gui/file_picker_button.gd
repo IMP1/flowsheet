14e0cb56f2fc3b4f32a1b95b0aab26e10c8560ab
IMP1 <huw_taylor@hotmail.co.uk>
2024-04-29 19:07:56 +0100

    Add filters and root subfolder to button object

--- project/gui/file_picker_button.gd
+++ project/gui/file_picker_button.gd
@@ -4,12 +4,26 @@ extends Button
 signal file_selected(path: String)
 
 @export var dialog: FileDialog
+@export var root: String:
+	set(value):
+		root = value
+		if dialog:
+			dialog.root_subfolder = root
+@export var filters: PackedStringArray:
+	set(value):
+		filters = value
+		if dialog:
+			dialog.filters = filters
 
 
 func _ready() -> void:
 	dialog.hide()
 	dialog.close_requested.connect(func(): button_pressed = false)
-	dialog.file_selected.connect(func(path: String): file_selected.emit(path))
+	dialog.file_selected.connect(func(path: String):
+		text = path.get_file()
+		file_selected.emit(path)
+		button_pressed = false)
+	dialog.root_subfolder = root
 
 
 func _toggled(toggled_on: bool) -> void:
