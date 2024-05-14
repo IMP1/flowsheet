26d8210c120a8b68ddb047aa002642a3ccef637a
IMP1 <huw_taylor@hotmail.co.uk>
2024-05-14 18:58:00 +0100

    Use same script context as scripts

--- project/util/console.gd
+++ project/util/console.gd
@@ -16,7 +16,8 @@ func _ready() -> void:
 		_input.clear()
 		run_command(text))
 	lua_context = LuaAPI.new()
-	_setup_lua_context(lua_context)
+	FlowsheetScriptContext.setup_context(lua_context, sheet)
+	#_setup_lua_context(lua_context)
 
 
 func _grab_focus() -> void:
@@ -58,40 +59,3 @@ func run_command(command: String) -> void:
 func _lua_print(message) -> void:
 	print(message)
 	log_message(str(message))
-
-
-func _setup_lua_context(context: LuaAPI) -> void:
-	# TODO: Maybe this should be a coroutine that has a hook which awaits a process frame every instruction?
-	#       Then lines can use nodes created in previous lines, it doesn't make flowsheet hang
-	#       The only downside is that the scripts will take longer to run
-	
-	context.object_metatable.permissive = false
-	# Object Constructors
-	context.push_variant("Vec2", func(x:float, y:float): return Vector2(x, y))
-	context.push_variant("Color", func(r:float, g:float, b:float, a:float): return Color(r, g, b, a))
-	# Constants
-	for t in FlowsheetNode.Type:
-		context.push_variant("NODE_TYPE_%s" % t, FlowsheetNode.Type.get(t))
-	
-	# Sheet Commands
-	context.push_variant("clear_sheet", sheet.clear_sheet)
-	context.push_variant("select", sheet.select_item)
-	context.push_variant("set_sheet_style", sheet.set_sheet_style)
-	
-	context.push_variant("add_node", sheet.add_node)
-	context.push_variant("remove_node", sheet.delete_node)
-	context.push_variant("set_node_type", sheet.set_node_type)
-	context.push_variant("set_node_editable", sheet.set_node_editable)
-	context.push_variant("set_node_initial_value", sheet.set_node_value)
-	context.push_variant("set_default_node_style", sheet.set_default_node_style)
-	context.push_variant("set_node_style", sheet.set_node_style)
-	
-	context.push_variant("add_link", sheet.add_link)
-	context.push_variant("remove_link", sheet.delete_link)
-	context.push_variant("set_link_formula", sheet.set_link_formula)
-	context.push_variant("set_default_link_style", sheet.set_default_link_style)
-	context.push_variant("set_link_style", sheet.set_link_style)
-	# TODO: Add more commands
-	# Misc
-	context.push_variant("print", _lua_print)
-
